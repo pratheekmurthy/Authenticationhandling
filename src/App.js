cb1277ea54fa207e154efe79d4c561f535d5f312
pratheekmurthy <pratheekkmurthy516@gmail.com>
2020-12-29 21:19:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {Link, BrowserRouter,Route} from 'react-router-dom'
+import Register from './register'
+import Home from './Home'
+import Login from './Login'
+
+
+
+const App=(props)=>{
+    return (<div> 
+        <BrowserRouter>
+        <Link to="/">Home</Link>|<Link to="/register">Register</Link>|<Link to="/login">Login</Link>
+        <Route path="/" component={Home} exact={true}/>
+        <Route path="/register" component={Register} exact={true}/>
+        <Route path="/login" component={Login} exact={true}/>
+        </BrowserRouter>
+    </div>)
+}
+
+export default App
